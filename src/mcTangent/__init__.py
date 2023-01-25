6e61bb92f2ad522a6616cf3b5a4cd4561136ece5
wglao <wesleygarlao@gmail.com>
2023-01-25 11:52:54 -0600

    remove unfinished file from init

--- src/mcTangent/__init__.py
+++ src/mcTangent/__init__.py
@@ -1,15 +1,15 @@
 from mcTangent import nn
 from mcTangent import mcTangent
-from mcTangent import mcT_setup
-from mcTangent import mcT_data
+# from mcTangent import mcT_setup
+# from mcTangent import mcT_data
 from mcTangent import solvers
 
 version = "0.0"
 
 __all__ = (
-    "mcT_data",
+    # "mcT_data",
     "mcTangent",
     "nn",
-    "mcT_setup",
+    # "mcT_setup",
     "solvers"
 )
