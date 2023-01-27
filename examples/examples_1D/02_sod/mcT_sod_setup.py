f54c9e73d21775076f115c505cba9f55c37f1c3b
wglao <wesleygarlao@gmail.com>
2023-01-27 13:36:18 -0600

    batch size robustness

--- examples/examples_1D/02_sod/mcT_sod_setup.py
+++ examples/examples_1D/02_sod/mcT_sod_setup.py
@@ -1,5 +1,8 @@
 from typing import Union, Tuple, NamedTuple, Optional, Iterable
-import os, functools, sys, copy
+import os
+import functools
+import sys
+import copy
 import json
 import pickle
 import wandb
@@ -25,7 +28,7 @@ config.update("jax_enable_x64", False)
 
 """parameters for initializing mcTangent"""
 os.environ["PROJ"] = '/home/wglao/Documents/PHO-ICES/mcT-JAXFLUIDS/examples/examples_1D/02_sod'
-proj = functools.partial(os.path.join,os.environ["PROJ"])
+proj = functools.partial(os.path.join, os.environ["PROJ"])
 save_path = proj('data')
 parallel_flag = False
 
@@ -66,23 +69,24 @@ noise_level = 0.02 if noise_flag else 0
 ns = 1
 nr = 1 if mc_flag else 0
 
+# sample set size
+num_train = 1
+num_test = 1
+test_ratio = 2
+
 num_epochs = int(3e4)
 learning_rate = 1e-3
 batch_size = 10
+batch_size = min(batch_size, num_train)
 layers = 1
 hidden_size = 5*nx
 activation = "relu"
-
-# sample set size
-num_train = 10
-num_test = 10
-test_ratio = 2
 
 # define batch by number of sequences trained on, instead of samples
 num_batches = int(np.ceil(num_train/batch_size))
 
 # edit case setup
-f = open(case_name+'.json','r')
+f = open(case_name+'.json', 'r')
 case_base = json.load(f)
 f.close()
 
@@ -97,7 +101,7 @@ seeds_to_gen = np.arange(num_test+num_train+1)
 # case_arr = np.empty(len(seeds_to_gen), dtype=object)
 
 # edit numerical setup
-f = open('numerical_setup.json','r')
+f = open('numerical_setup.json', 'r')
 numerical = json.load(f)
 f.close()
 
@@ -105,13 +109,15 @@ numerical['conservatives']['time_integration']['fixed_timestep'] = 0.1*dt
 numerical['conservatives']['convective_fluxes']['convective_solver'] = "FLUX-SPLITTING"
 numerical['mcTangent'] = 'false'
 
+
 class Cases():
     """
     Holds a stack of case setup dicts that can only be popped
     """
-    def __init__(self,cases: list):
+
+    def __init__(self, cases: list):
         self.cases = cases
-    
+
     def next(self) -> dict:
         """
         pops last case
@@ -120,25 +126,30 @@ class Cases():
             return self.cases.pop()
         except Exception as e:
             print(e)
-    
+
     def size(self):
         return len(self.cases)
 
 # random coefficients for initial conditions
-def pos_coefs(seed: int, out_shape: Union[tuple,int]) -> jnp.ndarray:
+
+
+def pos_coefs(seed: int, out_shape: Union[tuple, int]) -> jnp.ndarray:
     """
     provides an array of strictly positive coefficients
     """
     key = jrand.PRNGKey(seed)
     if type(out_shape) == int:
-        return jnp.abs(jnp.sqrt(0.5)*jrand.truncated_normal(key,-jnp.sqrt(0.5),jnp.sqrt(0.5),(out_shape,)) + 0.5) + jnp.finfo(jnp.float32).eps #strictly positive
-    return jnp.abs(jnp.sqrt(0.5)*jrand.truncated_normal(key,-jnp.sqrt(0.5),jnp.sqrt(0.5),out_shape) + 0.5) + jnp.finfo(jnp.float32).eps #strictly positive
+        # strictly positive
+        return jnp.abs(jnp.sqrt(0.5)*jrand.truncated_normal(key, -jnp.sqrt(0.5), jnp.sqrt(0.5), (out_shape,)) + 0.5) + jnp.finfo(jnp.float32).eps
+    # strictly positive
+    return jnp.abs(jnp.sqrt(0.5)*jrand.truncated_normal(key, -jnp.sqrt(0.5), jnp.sqrt(0.5), out_shape) + 0.5) + jnp.finfo(jnp.float32).eps
+
 
 def get_cases() -> Cases:
     case_list = []
     for seed in seeds_to_gen:
         case_new = copy.deepcopy(case_base)
-        coefs = pos_coefs(seed,3)
+        coefs = pos_coefs(seed, 3)
         rho0 = f"lambda x: 1.0*(x <= {coefs[2]}) + {coefs[0]}*(x > {coefs[2]})"
         p0 = f"lambda x: 1.0*(x <= {coefs[2]}) + {coefs[1]}*(x > {coefs[2]})"
         case_new['initial_condition']['rho'] = rho0
@@ -148,11 +159,14 @@ def get_cases() -> Cases:
 
     return Cases(case_list)
 
+
 cases = get_cases()
+
 
 @jit
 def _mse(array: jnp.ndarray):
     return jnp.mean(jnp.square(array))
+
 
 @jit
 def mse(pred: jnp.ndarray, true: Optional[jnp.ndarray] = None) -> float:
@@ -171,29 +185,34 @@ def mse(pred: jnp.ndarray, true: Optional[jnp.ndarray] = None) -> float:
         return _mse(pred)
     return _mse(pred - true)
 
-net = hk.without_apply_rng(mct.nn.create('dense',layers,hidden_size,activation,5*nx))
+
+net = hk.without_apply_rng(mct.nn.create(
+    'dense', layers, hidden_size, activation, 5*nx))
 optimizer = optax.eve()
 # optimizer = optax.adam(learning_rate)
+
 
 def save_params(params: hk.Params, path: str, filename: Optional[str] = None) -> None:
     # params = jax.device_get(params)
     if filename:
-        path = os.path.join(path,filename)
-    os.makedirs(os.path.dirname(path),exist_ok=True)
+        path = os.path.join(path, filename)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'wb') as fp:
         pickle.dump(params, fp)
         fp.close()
 
+
 def load_params(path: str, filename: Optional[str] = None):
     if filename:
-        path = os.path.join(path,filename)
+        path = os.path.join(path, filename)
     assert os.path.exists(path), "Specified parameter file does not exist"
     with open(path, 'rb') as fp:
         params = pickle.load(fp)
         fp.close()
     return params
 
-def compare_params(params: hk.Params, shapes: Union[Iterable[Iterable[int]],hk.Params]) -> bool:
+
+def compare_params(params: hk.Params, shapes: Union[Iterable[Iterable[int]], hk.Params]) -> bool:
     """
     Compares two sets of network parameters or a parameter dict with a list of prescribed shapes
     Returns True if the params dict has all correct shapes
@@ -214,14 +233,15 @@ def compare_params(params: hk.Params, shapes: Union[Iterable[Iterable[int]],hk.P
                     if params[ii][layer][wb].shape != shapes[2*jj+kk]:
                         return False
                 else:
-                    for a,b in zip(params[ii][layer][wb].shape,shapes[ii][layer][wb].shape):
+                    for a, b in zip(params[ii][layer][wb].shape, shapes[ii][layer][wb].shape):
                         if a != b:
                             return False
     return True
 
+
 if __name__ == "__main__":
     # uploading wandb
-    wandb.init(project="mcT-JAXFLUIDS",name="Warm Start")
+    wandb.init(project="mcT-JAXFLUIDS", name="Warm Start")
     wandb.config.problem = case_name
     wandb.config.mc_alpha = mc_alpha
     wandb.config.learning_rate = learning_rate
@@ -240,43 +260,48 @@ if __name__ == "__main__":
 
     import mcT_data as dat
     from mcT import get_coarse
-    
+
     cache_path = '.test_cache'
     # optimizer = optax.adam(1e-3)
 
     print('\n'+'-'*5+'Warm Start'+'-'*5+'\n')
-    
-    def warm_loss(params,cons_L,cons_R,truth):
+
+    def warm_loss(params, cons_L, cons_R, truth):
         net_tangent = jnp.zeros_like(cons_L)
         for i in range(5):
-            net_L = jax.jit(jax.vmap(net.apply, in_axes=(None,0)))(params[i],cons_L[:,i])
-            net_R = jax.jit(jax.vmap(net.apply, in_axes=(None,0)))(params[i],cons_R[:,i])
-            tangent_i = jnp.reshape(0.5*(net_L+net_R), net_tangent[:,i,...].shape)
-            net_tangent = net_tangent.at[:,i,...].set(tangent_i)
-        loss = mse(net_tangent*dx,truth)
+            net_L = jax.jit(jax.vmap(net.apply, in_axes=(None, 0)))(
+                params[i], cons_L[:, i])
+            net_R = jax.jit(jax.vmap(net.apply, in_axes=(None, 0)))(
+                params[i], cons_R[:, i])
+            tangent_i = jnp.reshape(
+                0.5*(net_L+net_R), net_tangent[:, i, ...].shape)
+            net_tangent = net_tangent.at[:, i, ...].set(tangent_i)
+        loss = mse(net_tangent*dx, truth)
         return loss
 
     def warm_load(sim: dat.Sim, sim_manager: SimulationManager, epoch: int):
         primes = jax.device_put(sim.load()[3])
         primes = jax.vmap(get_coarse, in_axes=(0))(primes)
-        primes = jnp.swapaxes(primes,0,1)
+        primes = jnp.swapaxes(primes, 0, 1)
         nh = numerical['conservatives']['halo_cells']
-        pad_dims = ((0,0),(0,0),(nh,nh),(0,0),(0,0))
-        primes = jnp.pad(primes,pad_dims,constant_values=1)
+        pad_dims = ((0, 0), (0, 0), (nh, nh), (0, 0), (0, 0))
+        primes = jnp.pad(primes, pad_dims, constant_values=1)
         primes_L = jnp.array(jax.vmap(
             sim_manager.space_solver.flux_computer.flux_computer.reconstruction_stencil.reconstruct_xi,
-            in_axes=(0,None,None,None))(primes, 0, 0, dx))
+            in_axes=(0, None, None, None))(primes, 0, 0, dx))
         primes_R = jnp.array(jax.vmap(
             sim_manager.space_solver.flux_computer.flux_computer.reconstruction_stencil.reconstruct_xi,
-            in_axes=(0,None,None,None))(primes, 0, 1, dx))
-        cons_L = jnp.array(jax.vmap(get_conservatives_from_primitives, in_axes=(0,None))(primes_L,sim_manager.material_manager))
-        cons_R = jnp.array(jax.vmap(get_conservatives_from_primitives, in_axes=(0,None))(primes_R,sim_manager.material_manager))
+            in_axes=(0, None, None, None))(primes, 0, 1, dx))
+        cons_L = jnp.array(jax.vmap(get_conservatives_from_primitives, in_axes=(
+            0, None))(primes_L, sim_manager.material_manager))
+        cons_R = jnp.array(jax.vmap(get_conservatives_from_primitives, in_axes=(
+            0, None))(primes_R, sim_manager.material_manager))
 
-        return primes_L,primes_R,cons_L,cons_R
+        return primes_L, primes_R, cons_L, cons_R
 
     def warm_start(epochs):
         # init net params
-        u_init = jnp.zeros((1,nx+1,ny,nz))
+        u_init = jnp.zeros((1, nx+1, ny, nz))
         params = net.init(jrand.PRNGKey(epochs), u_init)
         opt_state = [optimizer.init(params) for _ in range(5)]
         params = [params for _ in range(5)]
@@ -287,7 +312,7 @@ if __name__ == "__main__":
         for epoch in range(epochs):
             dat.data.check_sims()
             sim = dat.data.next_sim()
-            for _ in range(epoch%dat.data.size()):  # vary training data
+            for _ in range(epoch % dat.data.size()):  # vary training data
                 sim = dat.data.next_sim()
             case_dict = sim.case
             numerical = sim.numerical
@@ -296,49 +321,58 @@ if __name__ == "__main__":
             case_dict['domain']['x']['cells'] = nx
             numerical['conservatives']['time_integration']['fixed_timestep'] = dt
 
-            input_reader = InputReader(case_dict,numerical)
+            input_reader = InputReader(case_dict, numerical)
             sim_manager = SimulationManager(input_reader)
 
             # load data
-            primes_L,primes_R,cons_L,cons_R = warm_load(sim,sim_manager,epoch)
+            primes_L, primes_R, cons_L, cons_R = warm_load(
+                sim, sim_manager, epoch)
 
             # learn
-            model = HLLC(sim_manager.material_manager,signal_speed_Einfeldt)
+            model = HLLC(sim_manager.material_manager, signal_speed_Einfeldt)
             # model = Rusanov(sim_manager.material_manager,signal_speed_Einfeldt)
 
-            warm_true = jax.vmap(model.solve_riemann_problem_xi, in_axes=(0,0,0,0,None))
-            truth_array = jnp.array(warm_true(primes_L,primes_R,cons_L,cons_R,0))
-            loss, grads = jax.value_and_grad(jit(warm_loss),argnums=(0))(params,cons_L,cons_R,truth_array)
+            warm_true = jax.vmap(
+                model.solve_riemann_problem_xi, in_axes=(0, 0, 0, 0, None))
+            truth_array = jnp.array(
+                warm_true(primes_L, primes_R, cons_L, cons_R, 0))
+            loss, grads = jax.value_and_grad(jit(warm_loss), argnums=(0))(
+                params, cons_L, cons_R, truth_array)
             for i in range(5):
-                updates_i, opt_state[i] = jit(optimizer.update)(grads[i], opt_state[i])
+                updates_i, opt_state[i] = jit(
+                    optimizer.update)(grads[i], opt_state[i])
                 params[i] = jit(optax.apply_updates)(params[i], updates_i)
-            
+
             # test
             sim = dat.data.next_sim()
-            primes_L,primes_R,cons_L,cons_R = warm_load(sim,sim_manager,epoch)
-            test_truth = warm_true(primes_L,primes_R,cons_L,cons_R,0)
-            test_err = warm_loss(params,cons_L,cons_R,test_truth)
+            primes_L, primes_R, cons_L, cons_R = warm_load(
+                sim, sim_manager, epoch)
+            test_truth = warm_true(primes_L, primes_R, cons_L, cons_R, 0)
+            test_err = warm_loss(params, cons_L, cons_R, test_truth)
 
             if test_err < min_err:
                 epoch_min = epoch
                 min_err = test_err
 
             if epoch % 500 == 0 or epoch == epochs-1:
-                print("Loss {:.2e} TE {:.2e}  TE_min {:.2e} EPmin {:d} EP {} ".format(loss, test_err, min_err, epoch_min, epoch))
-            
+                print("Loss {:.2e} TE {:.2e}  TE_min {:.2e} EPmin {:d} EP {} ".format(
+                    loss, test_err, min_err, epoch_min, epoch))
+
             if epoch % 200 == 0 and epoch > 0:
                 jax.clear_backends()
-            wandb.log({"Train loss": float(loss), "Test Error": float(test_err), 'Test Min': float(min_err), 'Epoch' : float(epoch)})
+            wandb.log({"Train loss": float(loss), "Test Error": float(
+                test_err), 'Test Min': float(min_err), 'Epoch': float(epoch)})
             # clean up
-            os.system('rm -rf %s' %(cache_path))
-        
-        save_params(params,os.path.join(proj("network/parameters"),"warm.pkl"))
+            os.system('rm -rf %s' % (cache_path))
+
+        save_params(params, os.path.join(
+            proj("network/parameters"), "warm.pkl"))
 
     warm_epochs = 501
     warm_start(warm_epochs)
 else:
     # uploading wandb
-    wandb.init(project="mcT-JAXFLUIDS",name=case_name)
+    wandb.init(project="mcT-JAXFLUIDS", name=case_name)
     wandb.config.problem = case_name
     wandb.config.mc_alpha = mc_alpha
     wandb.config.learning_rate = learning_rate
