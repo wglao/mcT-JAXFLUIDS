70f0c3f88ae7d4d0b1ed325e82ca06bf695a1a73
= <=wesleygarlao@utexas.edu>
2023-01-27 13:38:11 -0600

    formatting, begin cnn implementation

--- src/mcTangent/nn.py
+++ src/mcTangent/nn.py
@@ -1,6 +1,10 @@
 from typing import Tuple, NamedTuple, Optional, Union, Iterable, Any
-import time, os, wandb
-import shutil, functools, warnings
+import time
+import os
+import wandb
+import shutil
+import functools
+import warnings
 import numpy as np
 import re
 import jax
@@ -25,38 +29,45 @@ warnings.simplefilter('default', UserWarning)
 mcTangent NN generator
 """
 
+
 def create(architecture: str = "DENSE", *a, **k):
     """
     Returns an mcTangent module based upon the input net architecture
     """
     if architecture.upper() not in DICT_ARCHITECTURES.keys():
-        warnings.warn(message = f"\nInvalid architecture type {architecture.upper()} requested. Choose from:\n{[key for key in DICT_ARCHITECTURES.keys()]}.\nReturning default dense net",
-                        category=UserWarning)
+        warnings.warn(message=f"\nInvalid architecture type {architecture.upper()} requested. Choose from:\n{[key for key in DICT_ARCHITECTURES.keys()]}.\nReturning default dense net",
+                      category=UserWarning)
         return DICT_ARCHITECTURES["DENSE"]()
     else:
         try:
             return DICT_ARCHITECTURES[architecture.upper()](*a, **k)
         except:
-            warnings.warn("\nInvalid architecture arguments given, returning default network of specified type")
+            warnings.warn(
+                "\nInvalid architecture arguments given, returning default network of specified type")
             return DICT_ARCHITECTURES[architecture.upper()]()
+
 
 class mcT_net_dense(hk.Module):
     """
     Creates a dense linear mcTangent NN with the specified number of layers
     """
-    def __init__(self, num_layers: int = 1, layer_sizes: Union[int,Iterable[int]] = 1, activations: Optional[Union[str,Iterable[str]]] = 'RELU',
-                    out_size: Union[int,Iterable[int]] = 1, name: Optional[str] = None):
+
+    def __init__(self, num_layers: int = 1, layer_sizes: Union[int, Iterable[int]] = 1, activations: Optional[Union[str, Iterable[str]]] = 'RELU',
+                 out_size: Union[int, Iterable[int]] = 1, name: Optional[str] = None):
         super().__init__(name=name)
 
         self.num_layers = num_layers
-        self.layer_sizes = [layer_sizes]*num_layers if type(layer_sizes) == int else layer_sizes
-        self.activations = [DICT_ACTIVATIONS[activations.upper()]]*num_layers if type(activations) == str else [DICT_ACTIVATIONS[fn.upper()] for fn in activations]
+        self.layer_sizes = [layer_sizes] * \
+            num_layers if type(layer_sizes) == int else layer_sizes
+        self.activations = [DICT_ACTIVATIONS[activations.upper()]]*num_layers if type(
+            activations) == str else [DICT_ACTIVATIONS[fn.upper()] for fn in activations]
         assert self.num_layers == len(self.layer_sizes)
         assert self.num_layers == len(self.activations)
 
-        self.out_size = out_size if type(out_size) == int else jnp.prod(out_size)
+        self.out_size = out_size if type(
+            out_size) == int else jnp.prod(out_size)
         self._create_net()
-    
+
     def __call__(self, input):
         out = self.net(jnp.ravel(input))
         return jnp.reshape(out, (input.shape))
@@ -72,100 +83,71 @@ class mcT_net_dense(hk.Module):
         self.net = hk.Sequential(sequence)
 
 # TODO: CNN
+
+
 # class mcT_net_cnn(hk.Module):
 #     """
-#     Creates an mcTangent NN with the specified number of layers
+#     Creates a CNN with the specified number of layers
 #     """
-#     def __init__(self, ndim: int = 1, num_layers: int = 1, layer_features: Union[int,Iterable[int]] = 1, kernel_shapes: Union[int,Iterable[Any]] = 1, output_shape: Union[int,Iterable[int]] = 1, name: Optional[str] = None):
+
+#     def __init__(
+#         self,
+#         ndims: int = 1,
+#         layers: int = 1,
+#         features: Union[int, Iterable[int]] = 1,
+#         kernel_shape: Union[int, Tuple[int]] = 3,
+#         stride: int = 1,
+#         activations: Optional[Union[str, Iterable[str]]] = 'RELU',
+#         out_size: int = 1,
+#         name: Optional[str] = None
+#     ):
 #         super().__init__(name=name)
 
-#         self.check_dims = functools.partial(self._check_dims,self,ndim)
-#         self.check_shapes = functools.partial(self._check_shapes,self,ndim)
+#         self.ndims = ndims
+#         self.layers = layers
 
-#         self.num_layers = num_layers
-
-#         self.layer_features = [layer_features]*num_layers if type(layer_features) == int else layer_features
-#         assert self.num_layers == len(self.layer_features)
-
-#         # use single input as edge length of ndim cube kernel for all layers
-#         if type(kernel_shapes) == int:
-#             self.kernel_shapes = [[[[[kernel_shapes]*ndim]*features] for features in range(layer)] for layer in self.layer_features]
-#         # use single iterable as shape for all layers and features, expanding if not all dims are given, truncating if too many
-#         elif type(kernel_shapes) == Iterable[int]:
-#             kernel_shape = self.check_dims(kernel_shapes)
-#             self.kernel_shapes = [[[[kernel_shape] * features] for features in layer] for layer in num_layers]
-#         # if iterable of shapes, check if sufficient count for num_layers
-#         elif type(kernel_shapes) == Iterable[Iterable[int]]:
-#             checked_shapes = []
-#             for features in layer_features:
-#                 checked_shapes = [self.check_shapes(len(features), self.check_dims(shape)) for features, shape
-#                 kernel_shapes = [self.check_dims(shape) for shape in kernel_shapes]
-#             self.kernel_shapes = kernel_shapes
-#         # if iterable of iterable of shapes, check if each layer has sufficient kernels for each feature
+#         # check if features is compatible with layer count
+#         if type(features) == int:
+#             self.features = [features] * layers
+#         elif len(features) != layers:
+#             print("feature count and layer count do not match")
+#             if len(features) > layers:
+#                 self.features = features[:layers]
+#             else:
+#                 feature_list = features
+#                 feature_list.append([1]*(layers-len(features)))
+#                 self.features = feature_list
 #         else:
-#             raise TypeError
-
-#         self.output_size = output_shape if type (output_shape) == int else jnp.prod(output_shape)
-#         self.output_shape = (output_shape,) if type (output_shape) == int else output_shape
-
-#         self.f = self._create_net()
-    
-#     def __call__(self, inputs, *a, **k):
-#         return self.f(inputs, *a, **k)
-    
-#     def _check_shapes(self, ndim, nshapes, shapes):
-#         missing_shapes = nshapes - len(shapes)
-#         shapes = [(shape.tolist()) for shape in shapes] if type(shapes) == jnp.ndarray else [(shape) for shape in shapes]
-#         if missing_shapes > 0:
-#             warnings.warn("Degenerate unit cube kernel used for last %s layers due to lack of input" %str(missing_shapes))
-#             shapes += [[1]*ndim for _ in range(missing_shapes)]
-#         elif missing_shapes < 0:
-#             warnings.warn("Truncating excessive kernel shapes. %s too many were given" %str(-missing_shapes))
-#             shapes = shapes[:missing_shapes]
-
-#     def _check_dims(self, ndim, shape):
-#         missing_dims = ndim - len(shape)
-#         if missing_dims > 0:
-#             pad_widths = (0,missing_dims)
-#             shape = jnp.pad(jnp.array(shape),pad_widths,"constant",constant_values=1)
-#         elif missing_dims < 0:
-#             warnings.warn("Truncating excessive kernel dimensions. %s too many were given" %str(-missing_dims))
-#             shape = shape[:missing_dims]
-#         return shape
+#             self.features = features
         
-#     def _create_net(self) -> callable:
-#         sequence = [hk.Flatten()]
-#         for layer, size in range(self.num_layers), self.layer_sizes:
-#             sequence += [hk.Linear(size), jax.nn.relu]
-#         sequence.append(hk.Linear(self.output_size))
-#         net = hk.Sequential(sequence)
-
-#         def f(x):
-#             out = net(x)
-#             return jnp.reshape(out, self.output_shape)
+#         # check if kernels are compatible with layers
+#         if type(kernel_shape) == int:
+#             self.kernel_shape = tuple([kernel_shape for dim in range(ndims)])
+#         elif len(kernel_shape) != ndims:
+#             print("kernel dims do not match network dims")
+#             if len(kernel_shape) > ndims:
+#                 self.kernel_shape = kernel_shape[:ndims]
+#             else:
+#                 kernel = list(kernel_shape)
+#                 kernel.append([1]*(ndims-len(kernel)))
+#                 self.kernel_shape = tuple(kernel)
+#         else:
+#             self.kernel_shape = kernel_shape
         
-#         return f
+#         # check if activations are compatible
+#         self.activations = activations
+#         self.out_size = out_size
 
 
+#     def __call__(self, state: jnp.ndarray) -> Any:
 
-def mcT_fn(state: jnp.ndarray) -> jnp.ndarray:
-    """Dense network with 1 layer of ReLU units"""
-    n_fields = state.shape[0]
-    n_faces = state.shape[1]
-    mcT = hk.Sequential([
-        hk.Flatten(),
-        hk.Linear(5*n_faces*n_fields), jax.nn.relu,
-        hk.Linear(n_faces)
-    ])
-    flux = mcT(state)
-    flux = jnp.reshape(flux,(n_fields,n_faces,1,1))
-    return flux
 
 def save_params(params, path):
     params = jax.device_get(params)
     os.makedirs(path)
     with open(path, 'wb') as fp:
         pickle.dump(params, fp)
+
 
 def load_params(path):
     assert os.path.exists(path), "Specified parameter save path does not exist"
@@ -175,16 +157,17 @@ def load_params(path):
 
 
 DICT_ARCHITECTURES = {
-    "DENSE" : lambda *args: hk.transform(lambda x: mcT_net_dense(*args)(x))
+    "DENSE": lambda *args: hk.transform(lambda x: mcT_net_dense(*args)(x))
     # "CONVOLUTIONAL" : lambda *args: hk.transform(lambda x: mcT_net_cnn(*args)(x)),
 }
 
 DICT_ACTIVATIONS = {
-    "RELU" : jax.nn.relu,
-    "TANH" : jax.nn.hard_tanh,
-    "ELU" : jax.nn.elu,
+    "RELU": jax.nn.relu,
+    "TANH": jax.nn.hard_tanh,
+    "ELU": jax.nn.elu,
     "NONE": None
 }
+
 
 class ScaleByEveState(NamedTuple):
     """State for the Eve algorithm."""
@@ -203,14 +186,15 @@ class ScaleByEveState(NamedTuple):
 #     self.f = 1
 #     self.f_star = min_global
 
+
 def scale_by_eve(b1: float = 0.9,
-    b2: float = 0.999,
-    b3: float = 0.999,
-    c: float = 10.,
-    eps: float = 1e-8,
-    f_star: float = 0.,
-    mu_dtype: Optional[Any] = None,
-) -> opbase.GradientTransformation:
+                 b2: float = 0.999,
+                 b3: float = 0.999,
+                 c: float = 10.,
+                 eps: float = 1e-8,
+                 f_star: float = 0.,
+                 mu_dtype: Optional[Any] = None,
+                 ) -> opbase.GradientTransformation:
     """Rescale updates according to the Eve algorithm.
 
     References:
@@ -241,14 +225,17 @@ def scale_by_eve(b1: float = 0.9,
         return ScaleByEveState(count=jnp.zeros([], jnp.int32), mu=mu, nu=nu, d=d, f=f)
 
     def update_fn(updates: opbase.Updates, state: ScaleByEveState, f: float):
-        mu = jtr.tree_map(lambda m, u: jnp.asarray(b1*m + (1-b1)*u), state.mu, updates)
-        nu = jtr.tree_map(lambda v, u: jnp.asarray(b1*v + (1-b1)*u), state.nu, updates)
+        mu = jtr.tree_map(lambda m, u: jnp.asarray(
+            b1*m + (1-b1)*u), state.mu, updates)
+        nu = jtr.tree_map(lambda v, u: jnp.asarray(
+            b1*v + (1-b1)*u), state.nu, updates)
         count_inc = oputils.numerics.safe_int32_increment(state.count)
         mu_hat = jtr.tree_map(lambda m: jnp.asarray(m / (1-b1)), mu)
         nu_hat = jtr.tree_map(lambda v: jnp.asarray(v / (1-b2)), nu)
         if count_inc > 1:
-            d_new = (jnp.abs(f - state.f)) / (jnp.min(jnp.array([f,state.f])) - f_star)
-            d_tilde = jnp.clip(d_new,1/c,c)
+            d_new = (jnp.abs(f - state.f)) / \
+                (jnp.min(jnp.array([f, state.f])) - f_star)
+            d_tilde = jnp.clip(d_new, 1/c, c)
             d = b3*state.d + (1-b3)*d_tilde
         else:
             d = 1.
@@ -258,6 +245,7 @@ def scale_by_eve(b1: float = 0.9,
         return updates, ScaleByEveState(count=count_inc, mu=mu, nu=nu, d=d, f=f)
 
     return opbase.GradientTransformation(init_fn, update_fn)
+
 
 def eve(
     learning_rate: float = 1e-3,
@@ -301,11 +289,12 @@ def eve(
         opalias._scale_by_learning_rate(learning_rate),
     )
 
+
 if __name__ == "__main__":
     # dense architecture: dense, layers, width, activations, in, out
-    net = create("dense",1,100,'relu',10,10)
+    net = create("dense", 1, 100, 'relu', 10, 10)
     print(net)
-    testx = jnp.linspace(0,1,10)
+    testx = jnp.linspace(0, 1, 10)
     params = net.init(jrand.PRNGKey(1), testx)
     print(testx)
     print(net.apply(params, None, testx))
